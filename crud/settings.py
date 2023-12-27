d97417ad200c32dfd764b347c4568d8c24fafb28
Data-Mind 2.0 <112772762+datamind321@users.noreply.github.com>
2023-12-27 15:33:02 +0530

    Update settings.py

--- crud/settings.py
+++ crud/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'crud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': '3B126Ga2F1-*B4gE6B2d56BDdgAd*53-',
-#         'HOST': 'monorail.proxy.rlwy.net',
-#         'PORT': '57700',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '3B126Ga2F1-*B4gE6B2d56BDdgAd*53-',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '57700',
+    }
+}
 
 
 
